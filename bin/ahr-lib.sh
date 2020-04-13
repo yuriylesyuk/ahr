1bc2f3074c7091502f454081108d0a5d51170c4e
Yuriy Lesyuk <ylesiuk@gmail.com>
2020-04-13 10:29:26 +0000

    Secure password input

--- bin/ahr-lib.sh
+++ bin/ahr-lib.sh
@@ -22,4 +22,20 @@ function check_envvars() {
     fi
 }
 
+function get_password(){
+    local password
+    local passconfirm
+    while true; do
+        read -srp "Enter password: " password
+        echo "" >&2
+        read -srp "Confirm password: " passconfirm
+        echo "" >&2
+        if [ "$password" == "$passconfirm" ]; then
+            break
+        else
+            echo "Passwords do not match. Please re-enter" >&2
+        fi
+    done
+    echo -n "$password"
+}
 
