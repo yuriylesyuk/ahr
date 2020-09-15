b139d8ce55912ebffeaee30c4b527a6e2f834ec8
Yuriy Lesyuk <yuriylesyuk@users.noreply.github.com>
2020-09-15 22:36:33 +0100

    Remove hardcoded apigeectl tarball file name

--- examples/hybrid-sz-s-1.3.sh
+++ examples/hybrid-sz-s-1.3.sh
@@ -20,7 +20,7 @@ export ASM_CONFIG=$HYBRID_HOME/istio-operator.yaml
 # Hybrid release configuration
 #
 export HYBRID_VERSION=1.3.2
-export HYBRID_TARBALL=apigeectl_linux_64.tar.gz
+export HYBRID_TARBALL=apigeectl_$(get_platform_suffix apigeectl $PLATFORM)"
 
 
 
